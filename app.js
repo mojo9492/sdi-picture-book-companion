1b9ac7ea84bc0ded93afdefd85c464a5de0c17a7
mojo9492 <mojo9492@outlook.com>
2021-08-17 14:57:40 -0700

    Add delete tests and routes

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
-    res.status(200).send({message: 'Welcome'})
+    res.status(200).send({ message: 'Hit the / route' })
+});
+
+app.get('/search', (req, res) => {
+    if (req.query[req.query.type]) {
+        //query database and send back results
+        console.log('req', req.query.type, req.query[req.query.type]);
+        res.status(200).send({ message: 'Hit the /results route' });
+
+    } else {
+        res.status(404)
+            .json({
+                message:
+                    'The data you are looking for could not be found. Please try again'
+            })
+    }
+});
+
+app.post('/add', (req, res) => {
+    const { name, data } = req.files.pic;
+
+    const errorMsg = 'The server could not process your request'
+    if (Object.values(req.body).includes(null) ||
+        Object.values(req.body).includes(undefined)) {
+        res.status(422).send({ message: errorMsg })
+    } else {
+        const { nomenclature, common, part, NSN, accounting, category, description } = req.body
+        //insert into database
+        res.status(200).send()
+    }
+
 })
 
-
-
-
+app.delete('/delete/:id', (req, res) => {
+    console.log('req.id', req.params.id)
+    if (req.params.id) {
+        res.status(200)
+    } else {
+        res.status(422).send({message: 'you got it'})
+    }
+})
 
 module.exports = app;
--- tests/index.test.js
+++ tests/index.test.js
@@ -16,33 +16,88 @@ describe('backend test', () => {
 
     });
 
-    describe('/', () => {
+    xdescribe('/', () => {
 
         it('serves a page', async () => {
             const response = await request(app)
                 .get('/')
 
-            expect(response.status).toEqual(200)
+            expect(response.status).toBe(200)
 
         });
 
     });
 
-    xdescribe('/results', () => {
+    xdescribe('/search', () => {
 
-        it('allows a post request', async () => {
-            const post = await request(app) // 3
-                .post('/results')
-                .send({ placeholder: 'somedata' })
-                .set('Accept', "application/json")
-            const response = await post;
-            expect(response.status).toEqual(200)
+        it('allows a query get request', async () => {
+            const queryType = 'NSN'
+            const queryNSN = '62400000272059'
+            const response = await request(app)
+                .get(`/search?q=${queryType}&${queryType}=${queryNSN}`);
+
+            expect(response.status).toBe(200)
+            expect(response.type).toBe('application/json')
+            expect(response.body.NSN).toBe(queryNSN)
         })
 
     });
 
+    describe('/add', () => {
+        const imageObject = { image_id: 1 }
+        const item = {
+            nomenclature: '',
+            common: '',
+            part: '',
+            NSN: '',
+            accounting: '',
+            category: '',
+            image: imageObject,
+            descripiton: ''
+        }
+
+        it('allows adding of a new item', async () => {
+            const response = await request(app)
+                .post('/add')
+                .send(item)
+
+            expect(response.status).toBe(200)
+            expect(response.body.NSN).toBe(item.NSN)
+        })
+
+        it('should return 422 if incorrect data is entered', async () => {
+            const message = 'The server could not process your request'
+            const badItem = {
+                nomenclature: 82783274,
+                common: 774374,
+                part: null,
+                NSN: undefined,
+                accounting: 772,
+                category: true,
+                image: null,
+                descripiton: ' '
+            }
+
+            const response = await request(app)
+                .post('/add')
+                .send(item)
+
+            expect(response.status).toBe(422)
+            expect(response.body.message).toBe(message)
+
+        })
+    })
+
+    describe('/delete', () => {
+        const itemId = 1
+
+        it('should delete', async () => {
+            const deleteResponse = await request(app)
+                .delete(`/delete/${itemId}`)
+            const deletionConfirmation = `You have delete item: ${deleteResponse.body.nomenclature}`
+
+            expect(deleteResponse.status).toBe(200)
+            expect(deleteResponse.body.message).toBe(deletionConfirmation)
+        })
+    })
 });
-
-
-
-
