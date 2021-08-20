a1b0f45f10f2cc9e34a80bc579819ed5202d810a
Nicholas Schafer <debug_bridge@outlook.com>
2021-08-21 00:27:09 +0900

    can upload an new item with image now =)

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -1,90 +1,80 @@
-
 import { useState } from 'react';
 import { useHistory }from'react-router-dom';
 
 const AddItem = () => {
 
-  const [category, setCategory] = useState(" ");
-  const [common, setCommon] = useState(" ");
-  const [nomenclature, setNomenclature] = useState(" ");
-  const [nsn, setNsn] = useState(" ");
-  const [part, setPart] = useState(" ");
-  const [description, setDescription] = useState(" ");
-  const [accounting, setAccounting] = useState(" ");
+const [category, setCategory] = useState(" ");
+const [common, setCommon] = useState(" ");
+const [nomenclature, setNomenclature] = useState(" ");
+const [nsn, setNsn] = useState(" ");
+const [part, setPart] = useState(" ");
+const [description, setDescription] = useState(" ");
+const [accounting, setAccounting] = useState(" ");
 
+const [selectedFile, setSelectedFile] = useState()
 
-  const [selectedFile, setSelectedFile] = useState()
+const onInputListeneCategory = (e) => {
+setCategory(e.target.value)
+}
+const onInputListenerCommon = (e) => {
+setCommon(e.target.value)
+}
+const onInputListenerNomenclature = (e) => {
+setNomenclature(e.target.value)
+}
+const onInputListenerNsn = (e) => {
+setNsn(e.target.value)
+}
+const onInputListenerPart = (e) => {
+setPart(e.target.value)
+}
+const onInputListenerDescription = (e) => {
+setDescription(e.target.value)
+}
+const onInputListenerAccounting = (e) => {
+setAccounting(e.target.value)
+}
+let imageFunc = (e) => {
+setSelectedFile(e.target.files[0])
 
-  const onInputListeneCategory = (e) => {
-    setCategory(e.target.value)
-  }
-  const onInputListenerCommon = (e) => {
-    setCommon(e.target.value)
-  }
-  const onInputListenerNomenclature = (e) => {
-    setNomenclature(e.target.value)
-  }
-  const onInputListenerNsn = (e) => {
-    setNsn(e.target.value)
-  }
-  const onInputListenerPart = (e) => {
-    setPart(e.target.value)
-  }
-  const onInputListenerDescription = (e) => {
-    setDescription(e.target.value)
-  }
-  const onInputListenerAccounting = (e) => {
-    setAccounting(e.target.value)
-  }
-  let imageFunc = (e) => {
-    setSelectedFile(e.target.files[0])
-
-  }
-
-  const postFunc = (e) => {
-    const formData = new FormData();
-
-
-
-  let data ={
-    "nomenclature": nomenclature,
-    "common": common,
-    "part_number": part,
-    "nsn": nsn,
-    "accounting": accounting,
-    "category": category,
-    "description": description
 }
 
-formData.append('File',selectedFile)
-formData.append('item', data)
+const postFunc = (e) => {
+e.preventDefault();
 
-fetch('http://localhost:8080/add', {
-  method:'POST',
-  body: formData,
+const fd = new FormData(document.getElementById("myForm"));
+
+let url = 'http://localhost:8080/add';
+let req = new Request(url, {
+body: fd,
+method: 'POST',
+});
+
+fetch(req)
+.then(res => res.json())
+.then(data => {
+console.log('response from serer');
+console.log(data);
 })
-  .then(res => console.log(res))
-  .then(input => console.log(selectedFile))
-  e.preventDefault()
+.catch(console.warn);
 
-    }
+}
 
 
-
-  return (
-    <>
-    <form onSubmit={ postFunc }>
-      <input type='text' placeholder='Category' onInput={ onInputListeneCategory }/>
-      <input type='text' placeholder='Common Name' onInput={ onInputListenerCommon }/>
-      <input type='text' placeholder='Nomenclature' onInput={ onInputListenerNomenclature }/>
-      <input type='text' placeholder='NSN' onInput={ onInputListenerNsn }/>
-      <input type='text' placeholder='Accounting' onInput={ onInputListenerAccounting }/>
-      <input type='text' placeholder='Part Number' onInput={ onInputListenerPart }/>
-      <input type='text' placeholder='Description' onInput={ onInputListenerDescription }/>
-      <input type='file' name='file' onChange = {imageFunc} />
-      <input type='button' value='Submit' onClick={ postFunc }/>
-    </form>
-  </>
+return (
+<>
+<form onSubmit={ postFunc } id="myForm">
+<input type='text' name='nomenclature' onInput={ onInputListenerNomenclature }/>
+<input type='text' name='common' onInput={ onInputListenerCommon }/>
+<input type='text' name='part_number' onInput={ onInputListenerPart }/>
+<input type='text' name='nsn' onInput={ onInputListenerNsn }/>
+<input type='text' name='accounting' onInput={ onInputListenerAccounting }/>
+<input type='text' name='category' onInput={ onInputListeneCategory }/>
+<input type='text' name='description' onInput={ onInputListenerDescription }/>
+<input type='file' name='file' onChange = {imageFunc} />
+<input type='button' name='image' value='Submit' onClick={ postFunc }/>
+</form>
+</>
 );
 
 }
